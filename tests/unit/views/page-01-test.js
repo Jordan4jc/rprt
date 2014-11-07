b18652a95ec867868f35039acece3aae443f2e46
Jordan R <riser.jordan@gmail.com>
2014-11-07 15:33:13 -0500

    added view tests

---
+++ tests/unit/views/page-01-test.js
@@ -0,0 +1,12 @@
+import {
+  moduleFor,
+  test
+} from 'ember-qunit';
+
+moduleFor('view:page-01', 'Page01View');
+
+// Replace this with your real tests.
+test('it exists', function() {
+  var view = this.subject();
+  ok(view);
+});
