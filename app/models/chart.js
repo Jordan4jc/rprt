d01dd560edca469f5b3c5f34c64b76250da6e2fe
Jordan R <riser.jordan@gmail.com>
2014-11-10 11:44:50 -0500

    added example chart data

--- app/models/chart.js
+++ app/models/chart.js
@@ -6,7 +6,7 @@ export default DS.Model.extend({
     labels: ["January", "February", "March", "April", "May", "June", "July"],
     datasets: [
         {
-            label: "Social Data for October",
+            label: "My First dataset",
             fillColor: "rgba(220,220,220,0.2)",
             strokeColor: "rgba(220,220,220,1)",
             pointColor: "rgba(220,220,220,1)",
@@ -14,6 +14,16 @@ export default DS.Model.extend({
             pointHighlightFill: "#fff",
             pointHighlightStroke: "rgba(220,220,220,1)",
             data: [65, 59, 80, 81, 56, 55, 40]
+        },
+        {
+            label: "My Second dataset",
+            fillColor: "rgba(151,187,205,0.2)",
+            strokeColor: "rgba(151,187,205,1)",
+            pointColor: "rgba(151,187,205,1)",
+            pointStrokeColor: "#fff",
+            pointHighlightFill: "#fff",
+            pointHighlightStroke: "rgba(151,187,205,1)",
+            data: [28, 48, 40, 19, 86, 27, 90]
         }
     	]
 		};
