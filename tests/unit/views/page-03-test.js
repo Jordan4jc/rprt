ccc9cc6ef113185501b43880dcabf3a30d644e35
Jordan R <riser.jordan@gmail.com>
2014-11-18 14:16:42 -0500

    page 3 tests generated from ember cli

---
+++ tests/unit/views/page-03-test.js
@@ -0,0 +1,12 @@
+import {
+  moduleFor,
+  test
+} from 'ember-qunit';
+
+moduleFor('view:page-03', 'Page03View');
+
+// Replace this with your real tests.
+test('it exists', function() {
+  var view = this.subject();
+  ok(view);
+});
