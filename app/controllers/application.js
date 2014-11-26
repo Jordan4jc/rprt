dacc86b9061e5209fb1b297f7dd1a91ce2e0c490
Jordan R <riser.jordan@gmail.com>
2014-11-26 18:18:15 -0500

    added properties for month and year to parse and set them in the application template

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -1,6 +1,24 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+	monthToParse: null,
+	yearToParse: null,
+
+	fullDateToParse: function() {
+		// set array to month names
+		var monthNames = [ "January", "February", "March", "April", "May", "June",
+	  "July", "August", "September", "October", "November", "December" ];
+		// set a variable to a new date object
+		var date = new Date();
+		// set the month to the previous month and return that
+		date.setMonth(date.getMonth() - 1);
+		// set the controller properties for use in other parts of the app
+		this.set('monthToParse', monthNames[date.getMonth()]);
+		this.set('yearToParse', date.getFullYear());
+
+		return this.get('monthToParse') + ' ' + this.get('yearToParse');
+	}.property('monthToParse', 'yearToParse'),
+
 	actions: {
 		toggleOptions: function() {
 			Ember.$('nav.options-bar').toggleClass('closed');
