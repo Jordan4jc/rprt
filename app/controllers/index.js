23820691110f928767d68d7efce62394ff8cf08a
Jordan Riser <riser.jordan@gmail.com>
2015-05-04 14:05:45 -0400

    added bar graph component for twitter

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -31,5 +31,31 @@ export default Ember.ArrayController.extend({
 			metricStyle: "width: 79%"
 		}
 	],
-	desktop_sessions_bar_text: [0,221]
+	desktop_sessions_bar_text: [0,221],
+	twitter: [
+		{
+			metricNumber: "0",
+			metricLabel: "12AM-7AM",
+			metricClass: "metric blue__shade-3",
+			metricStyle: "width: 5%"
+		},
+		{
+			metricNumber: "0",
+			metricLabel: "7AM-12PM",
+			metricClass: "metric blue__shade-2",
+			metricStyle: "width: 5%"
+		},
+		{
+			metricNumber: "2",
+			metricLabel: "12PM-5PM",
+			metricClass: "metric blue__shade-1",
+			metricStyle: "width: 12%"
+		},
+		{
+			metricNumber: "97",
+			metricLabel: "5PM-12AM",
+			metricClass: "metric blue",
+			metricStyle: "width: 78%"
+		}
+	],
 });
