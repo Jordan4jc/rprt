a48f2e91b48a28a8ab780025cbe287dd32ba901f
Jordan Riser <riser.jordan@gmail.com>
2015-05-05 14:53:23 -0400

    changed index controller to object controller

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,6 +1,6 @@
 import Ember from 'ember';
 
-export default Ember.ArrayController.extend({
+export default Ember.ObjectController.extend({
 	report_month: "April",
 	report_last_month: "March",
 	report_year: 2015,
